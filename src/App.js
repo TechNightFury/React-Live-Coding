b964f5fd1f0c5eba75208f9f9cac13e7e8380e6a
TechNightFury <onni.tan62@gmail.com>
2024-11-06 15:40:32 -0800

    feat: set the routing.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './pages/Layout';
 import Counter from './pages/Counter';
 import Toggle from './pages/Toggle';
 import Todos from './pages/Todos';
+import DataFetcher from './pages/DataFetcher';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='counter' element={<Counter />} />
         <Route path='toggle' element={<Toggle />} />
         <Route path='todos' element={<Todos />} />
+        <Route path='data-fetch' element={<DataFetcher />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/DataFetcher.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DataFetcher = () => {
+    return (
+        <>
+            <Link to='/'>Back</Link>
+            <header>
+                This is data fetcher commponent
+            </header>
+            <main>
+
+            </main>
+        </>
+    )
+};
+
+export default DataFetcher;
