9e0968f5ba138d81fb9c8f20b39001d40bb480a3
TechNightFury <onni.tan62@gmail.com>
2024-11-06 01:03:32 -0800

    feat: completed the counter component

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
     return (
         <>
             <header>
-                <h3>Create a counter.</h3>
+                <h3>Create a counter component.</h3>
+                <small>Create a simple counter component that increase or decrease the count when clicking buttons.</small>
             </header>
+            <div>
+                <p>Count: {count}</p>
+                <button onClick={() => setCount(count + 1)}>Increase</button>
+                <button onClick={() => setCount(count - 1)}>Decrease</button>
+            </div>        
         </>
     )
 };
