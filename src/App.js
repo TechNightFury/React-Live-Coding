3fd2cc308ae02ef1d79037d9510fa309f23306f4
TechNightFury <onni.tan62@gmail.com>
2024-11-06 00:50:48 -0800

    feat: completed the initial counter component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Layout from './components/Layout';
+import Layout from './pages/Layout';
+import Counter from './pages/Counter';
 
 function App() {
   return (
     <Router>
-      <Routes path='/' element={Layout}>
-        <Route path='/' />
-        <Route path='/' />
-        <Route path='/' />
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route path='counter' element={<Counter />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Counter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Counter = () => {
+    return (
+        <>
+            <header>
+                <h3>Create a counter.</h3>
+            </header>
+        </>
+    )
+};
+
+export default Counter;
