702d9b642e846b28185ad1d33a11c04924eb2cf1
TechNightFury <onni.tan62@gmail.com>
2024-11-06 19:08:19 -0800

    feat: add back button, title, problem on Searchbar component

--- src/pages/Searchbar.js
+++ src/pages/Searchbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchBar = ({items}) => {
     const [query, setQuery] = useState('');
@@ -6,21 +7,29 @@ const SearchBar = ({items}) => {
 
     return (
         <>
-            <input 
-                value={query}
-                onChange={e => setQuery(e.target.value)}
-                placeholder='Search...'
-            />
+            <header>
+                <Link to='/'>Back</Link>
+                <h3>Create a Search Bar</h3>
+                <small>Create a search bar component that filters a list of items as the user types</small>
+            </header>
+            <hr />
+            <main>
+                <input 
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                    placeholder='Search...'
+                />
 
-            <ul>
-                {
-                    filteredItems.length ? filteredItems.map((item, index) => (
-                        <li keys={index}>
-                            {item}
-                        </li>
-                    )) : <p>No search results</p>
-                }
-            </ul>
+                <ul>
+                    {
+                        filteredItems.length ? filteredItems.map((item, index) => (
+                            <li keys={index}>
+                                {item}
+                            </li>
+                        )) : <p>No search results</p>
+                    }
+                </ul>
+            </main>
         </>
     )
 };
