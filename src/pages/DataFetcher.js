9dbbf0a3c3e0a20e6cbc74a23990cf8f7d4d0ca0
TechNightFury <onni.tan62@gmail.com>
2024-11-06 17:48:01 -0800

    feat: complete component for fetching data from API

--- src/pages/DataFetcher.js
+++ src/pages/DataFetcher.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
 const DataFetcher = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        console.log('loading...');
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(data);
+                setLoading(true);
+            });
+    }, []);
+
     return (
         <>
             <Link to='/'>Back</Link>
             <header>
-                This is data fetcher commponent
+                <h3>Fetch Data from an API</h3>
+                <small>Create a component fetching data from an API and displaying it in a list.</small>
             </header>
             <main>
-
+                <p>{ loading ? 'Received!' : 'Loading...' }</p>
+                <ul>
+                {
+                    data.map((item, index) => (
+                        <li key={index}>
+                            {item.title}
+                        </li>
+                    ))
+                }
+                </ul>
             </main>
         </>
     )
