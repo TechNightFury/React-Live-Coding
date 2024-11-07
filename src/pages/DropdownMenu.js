ca05f7c62e580bcd9e58987321635633a3ce025f
TechNightFury <onni.tan62@gmail.com>
2024-11-06 19:08:58 -0800

    feat: completed the dropdown menu  component

---
+++ src/pages/DropdownMenu.js
@@ -0,0 +1,28 @@
+
+import React, {useState} from "react";
+
+const DropdownMenu = ({items}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setIsOpen(!isOpen)}>Dropdown Menu</button>
+            {
+                isOpen && 
+                <ul>
+                    {
+                        items.map((item, index) => (
+                            <li key={index}>item</li>
+                        ))
+                    }
+                </ul>
+            }
+        </>
+    ) 
+};
+
+const App = () => {
+    const items = ['Apple', 'Pear', 'Melon', 'Cherry', 'Mango', 'Grape'];
+    
+    return <DropdownMenu items = {items} />
+};
