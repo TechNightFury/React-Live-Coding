6bcd927194d41bde93a25f21c281dabd5f794deb
TechNightFury <onni.tan62@gmail.com>
2024-11-06 19:47:44 -0800

    feat: add some function on dropdown component

--- src/pages/DropdownMenu.js
+++ src/pages/DropdownMenu.js
@@ -3,28 +3,33 @@ import React, {useState} from "react";
 
 const DropdownMenu = ({items}) => {
     const [isOpen, setIsOpen] = useState(false);
-
+    const [btnName, setBtnName] = useState('');
     return (
         <>
-            <button onClick={() => setIsOpen(!isOpen)}>Dropdown Menu</button>
+            <button onClick={() => setIsOpen(!isOpen)}>{!btnName ? 'Dropdown Menu' : btnName}</button>
             {
-                isOpen && 
-                <ul>
-                    {
-                        items.map((item, index) => (
-                            <li key={index}>
-                                {item}
-                            </li>
-                        ))
-                    }
-                </ul>
+                isOpen &&
+                <div>
+                    <ul>
+                        {
+                            items.map((item, index) => (
+                                <li key={index} onClick={() => {
+                                    setIsOpen(false);
+                                    setBtnName(item);
+                                }}>
+                                    {item}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div> 
             }
         </>
     ) 
 };
 
 const App = () => {
-    const items = ['Apple', 'Pear', 'Melon', 'Cherry', 'Mango', 'Grape'];
+    const items = ['', 'Apple', 'Pear', 'Melon', 'Cherry', 'Mango', 'Grape'];
     
     return <DropdownMenu items = {items} />
 };
